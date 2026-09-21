bfc4202b10115f8f1fc3a6be9731a2b75ef0eddd
mav-dorgan <mavvegg@gmail.com>
2026-09-21 00:21:09 -0300

    Pedido de informacion a la API

--- js/trivia.js
+++ js/trivia.js
@@ -2,11 +2,8 @@
 // SELECCION DE LOS ELEMENTOS DEL DOM
 //===================================
 
-//pedido de la API
-const url = "https://api.restcountries.com/countries/v5";
-const key = MI_API_KEY; // esta api key esta restringida a ser usada solo con ciertas paginas como mi pagina de github y el pages
-
 //elementos del html para modificar su contenido y visibilidad
+
 const inicio = document.querySelector ('#triviaInicio');
 const botonComenzar = document.querySelector ('#triviaComenzar');
 
@@ -27,26 +24,75 @@ const puntaje = document.querySelector ('#triviaPuntaje');
 const jugador = document.querySelector ('#triviaJugador');
 const botonReiniciar = document.querySelector ('#triviaReiniciar');
 
+//=======================================
+//JUEGO PRINCIPAL: CREACION DE PREGUNTAS
+//=======================================
 
-//hago el pedido de los paises con sus respectivas banderas y los guardo en un array de objetos
+////IDEA PRINCIPAL: hago el pedido de los paises con sus respectivas banderas y los guardo en un array de objetos
 
-let paises [];
+// La API solo me permite pedir de hasta 25 paises asi que debo hacer varios pedidos agregando un offset que corre a los siguiente 25 paises
+let offset = 0;
+const key = "MI_API_KEY"; // esta api key esta restringida a ser usada solo con ciertas paginas como la pagina de github pages de este repositorio, por lo que no es un problema publicarla
 
-fetch (url, {
-    headers: {
-        "Authorization": "Bearer " + key
-    }
-})
+let paises = [];
+let paisesConBandera = [];
+
+//la funcion pide a la Api 25 paises, los convierte a formato json y los alamcena en el array de paises. Luego pregunta si quedan mas paises por pedir, si la respuesta es si, se agrega un +25 al offset y se hace un nuevo pedido. Esta suma al offset permita que se pida a partir del pais 26 y no se repitan los mismos de antes.
+
+function cargarPaises() {
+    const url = "https://api.restcountries.com/countries/v5?offset=" + offset;
+    
+    fetch(url, {
+        headers: {
+            "Authorization": "Bearer " + key
+        }
+    })
     .then(function (respuesta) {
         return respuesta.json();
     })
     .then(function (datos) {
-        paises = datos
-        console.log(datos);
-    });
 
+        datos.data.objects.forEach(function (pais) {
+                paises.push(pais);
+        });
 
-console.log(paises[0]);
-console.log(paises[0].name);
-console.log(paises[0].name.translations.spa);
-console.log(paises[0].flags);
+        console.log("Países cargados:", paises.length);
+
+        if (datos.data.meta.more == true && paises.length < 250) {
+            offset = offset + 25;
+            cargarPaises();
+
+        } else {
+
+            console.log("Todos los países fueron cargados");
+            console.log(paises.length);
+            
+            //Como no todos los paises tienen cargada una bandera hago un if que recorra el array y se quede solo con aquellos que tienen una foto de la bandera.
+            paises.forEach (function (pais) {
+            if (pais.flag.url_png != "") {
+                paisesConBandera.push(pais);
+            }})
+            console.log (paisesConBandera.length);
+        }})
+}
+cargarPaises ();
+
+function generarPregunta () {
+    let opcionesPreguntas = [];
+
+    //Se determina aleatoriamente el pais correcto
+    let numeroCorrecto = Math.floor(Math.random()*paisesConBandera.length);
+    let paisCorrecto = paisesConBandera[numero];
+
+    opcionesPreguntas.push(paisCorrecto);
+
+    //Se deterinan los otros paises para las opciones incorrectas, corroborando que no se repita el mismo pais de la opcion correcta.
+    while (opcionesPreguntas.length < 4) {
+        let numeroIncorrecto = Math.floor(Math.random()*paisesConBandera.length);
+        let paisIncorrecto = paisesConBandera[numeroIncorrecto];
+
+        if (paisIncorrecto != paisCorrecto) {
+            opcionesPreguntas.push(paisIncorrecto);
+        }
+    }
+}
