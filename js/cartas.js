ab7e0b51e5356907022cde58314080e97ee48754
Gabriella La Rosa <gabriellalarosam@gmail.com>
2026-09-23 00:17:31 -0300

    Agrego campos donde el usuario ingresa el nivel dificultad y su nombre en el HTML. Además genero las cartas del tablero de forma dinamica con js en función del nivel de dificultad.

--- js/cartas.js
+++ js/cartas.js
@@ -1,6 +1,29 @@
 // Capturo el contenedor del HTML donde iran las cartas
 let tablero = document.querySelector("#tableroCartas");
+let btnComenzar = document.querySelector("#memotestComenzar");
 
-for(i=0; i<12; i++){
-    tablero.innerHTML += `<button class="carta">?</button>`
-}
+btnComenzar.addEventListener("click", function(){
+    tablero.innerHTML = "";
+
+    let nivelDificultad = document.querySelector("#nivel").value;
+
+    switch(nivelDificultad){
+        case "facil":
+            for(i=1; i<=12; i++){
+                tablero.innerHTML += `<img src="/img/cartas/memotest-11.jpg" alt="Imagen de interrogante" width="160" height="160">`
+            }
+            break;
+
+        case "medio":
+            for(i=1; i<=16; i++){
+                tablero.innerHTML += `<img src="/img/cartas/memotest-11.jpg" alt="Imagen de interrogante" width="160" height="160">`
+            }
+            break;
+
+        default:
+            for(i=1; i<=20; i++){
+                tablero.innerHTML += `<img src="/img/cartas/memotest-11.jpg" alt="Imagen de interrogante" width="160" height="160">`
+            }
+            break;
+    }
+})
