4133dfe2bfccc0733bb3b77ee4f27b30f272876c
mav-dorgan <mavvegg@gmail.com>
2026-09-24 23:29:56 -0300

    Trivia: progreso, vidas y puntajes
    Aumento puntos o disminuyo vidas dependiendo de si la respuesta es contestada correctamente.
    Agrego un contador de pregunta

--- js/trivia.js
+++ js/trivia.js
@@ -33,16 +33,20 @@ const botonReiniciar = document.querySelector ('#triviaReiniciar');
 
 // La API solo me permite pedir de hasta 25 paises asi que debo hacer varios pedidos agregando un offset que corre a los siguiente 25 paises
 let offset = 0;
-const key = "MI_API_KEY"; // esta api key esta restringida a ser usada solo con ciertas paginas como la pagina de github pages de este repositorio, por lo que no es un problema publicarla
+const key = 'MI_API_KEY'; // esta api key esta restringida a ser usada solo con ciertas paginas como la pagina de github pages de este repositorio, por lo que no es un problema publicarla
 
 let paises = [];
 let paisesConBandera = [];
 let paisesUsados = []; //almacena los paises cuyas banderas se usaron para evitar repetir preguntas
 
+let vidasJugador = 3;
+let puntos = 0;
+let numeroPregunta = 0;
+
 //la funcion pide a la Api 25 paises, los convierte a formato json y los alamcena en el array de paises. Luego pregunta si quedan mas paises por pedir, si la respuesta es si, se agrega un +25 al offset y se hace un nuevo pedido. Esta suma al offset permita que se pida a partir del pais 26 y no se repitan los mismos de antes.
 async function cargarPaises() {
     try {
-        const url = "https://api.restcountries.com/countries/v5?offset=" + offset;
+        const url = 'https://api.restcountries.com/countries/v5?offset=' + offset;
 
         const respuesta = await fetch(url, {
             headers: {
@@ -52,7 +56,7 @@ async function cargarPaises() {
 
         //Verifica que no haya errores
         if (!respuesta.ok) {
-            throw new Error("No se pudieron cargar los países");
+            throw new Error('No se pudieron cargar los países');
         }
 
         const datos = await respuesta.json();
@@ -62,7 +66,7 @@ async function cargarPaises() {
             paises.push(pais);
         });
 
-        console.log("Países cargados:", paises.length);
+        console.log('Países cargados:', paises.length);
 
         if (datos.data.meta.more == true && paises.length < 250) {
 
@@ -71,7 +75,7 @@ async function cargarPaises() {
 
         } else {
 
-            console.log("Todos los países fueron cargados");
+            console.log('Todos los países fueron cargados');
             console.log(paises.length);
 
             //Como no todos los paises tienen cargada una bandera hago un if que recorra el array y se quede solo con aquellos que tienen una foto de la bandera.
@@ -86,30 +90,9 @@ async function cargarPaises() {
             let pregunta = generarPregunta();
             let paisCorrecto = pregunta.correcto; //Almaceno la respuesta correcta en su propia variable
             
-            bandera.innerHTML = '<img src="' + pregunta.correcto.flag.url_png + '">'; //muestro en el HTML la bandera del pais correcto
-            
-            pregunta.opciones.forEach(pais => {
-            opciones.innerHTML += '<button type="button" name="' + pais.names.translations.spa.common + '">' + pais.names.translations.spa.common + '</button>';
-            });
-            console.log(opciones.innerHTML);
-
-            let botones = opciones.querySelectorAll('button');
-
-            botones.forEach(boton => {
-                boton.addEventListener('click', function () {
-                    if (boton.name == pregunta.correcto.names.translations.spa.common) {
-                        console.log("Correcto");
-                    } else {
-                        console.log("Incorrecto");
-                    }
-                });
-            });
         }
-
     } catch (error) {
-
-        console.log("Ocurrió un error:", error);
-
+        console.log('Ocurrió un error:', error);
     }
 }
 
@@ -117,7 +100,13 @@ cargarPaises ();
 
 //Creo una funcion para generar los 4 paises utilizados en la opcion multiple, incluyendo el pais con la bandera correcta
 function generarPregunta () {
-    
+
+    numeroPregunta ++;
+
+    progreso.innerHTML = 'Pregunta ' + numeroPregunta;
+    vidas.innerHTML = 'Vidas: ' + vidasJugador;
+    contadorCorrectas.innerHTML = 'Puntos: ' + puntos;
+ 
     let opcionesPregunta = [];
 
     let paisesDisponibles = buscarDisponibles (paisesConBandera);
@@ -133,18 +122,18 @@ function generarPregunta () {
         opcionesPregunta.push(pais);
     });
 
-    /*console.log (paisCorrecto.names.translations.spa.common);        
-    opcionesPregunta.forEach(pais => {
-        console.log(pais.names.translations.spa.common);
-    });*/
-
     let opcionesMezcladas = mezclar(opcionesPregunta);
-    //console.log (opcionesMezcladas);
-    
-    return {
+
+    let pregunta = {
     opciones: opcionesMezcladas,
     correcto: paisCorrecto
     };
+
+    //muestro en el HTML la bandera a adivinar
+    bandera.innerHTML = '<img src="' + pregunta.correcto.flag.url_png + '">';
+    mostrarOpciones(pregunta);
+
+    return pregunta;
 }
 
 
@@ -199,3 +188,58 @@ function buscarIncorrectos(arreglo, correcto) {
 function mezclar(arreglo) {
     return [...arreglo].sort(() => Math.random() - 0.5);
 }
+
+//Creo 4 botones en el HTML con las opciones. Al hacer click en alguno se deshabilitan todos. Finalmente corroboro la respuesta correcta y dependiendo el resultado se suman puntos o se resta una vida.
+function mostrarOpciones(pregunta) { 
+
+    opciones.innerHTML = "";
+
+    //Creo los 4 botones correspondientes a cada pais
+    pregunta.opciones.forEach(pais => {
+        opciones.innerHTML += '<button type="button" name="' + pais.names.translations.spa.common + '">' + pais.names.translations.spa.common + '</button>';
+    });
+
+    let botones = opciones.querySelectorAll('button');
+
+    //Corroboro la respuesta
+    botones.forEach(boton => {
+        boton.addEventListener('click', function () {
+
+            if (boton.name == pregunta.correcto.names.translations.spa.common) {
+
+                puntos += 100;
+                console.log('Correcto');
+                console.log('Puntos:', puntos);
+
+            } else {
+
+                vidasJugador -= 1;
+                console.log('Incorrecto');
+                console.log('Vidas:', vidasJugador);
+
+            }
+            
+            //Actualizo el contador de vidas y puntaje
+            vidas.innerHTML = 'Vidas: ' + vidasJugador;
+            contadorCorrectas.innerHTML = 'Puntos: ' + puntos;
+
+            //Deshabilito los botones
+            botones.forEach(boton => {
+                boton.disabled = true;
+            });
+            
+            //habilito el boton para la siguiente pregunta
+            botonSiguiente.hidden = false;
+
+        });
+    });
+};
+
+//Habilito la funcionalidad del boton para la siguiente pregunta
+botonSiguiente.addEventListener('click', function () {
+    
+    if (vidasJugador > 0) {
+    generarPregunta();
+    botonSiguiente.hidden = true;
+    };
+});
